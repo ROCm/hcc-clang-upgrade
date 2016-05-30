ad834c5da2184b275a0ed7d578be244b7de2cae7
Jack Chung <whchung@gmail.com>
2016-05-30 16:25:22 +0800

    [Sema] remove compile-time warning
    Adopted from Clang 3.5-based HCC-clang frontend.
    
    https://github.com/RadeonOpenCompute/hcc-clang/commit/7dd9258
    
    - lib/Sema/SemaDeclCXX.cpp : Patched as-is

--- lib/Sema/SemaDeclCXX.cpp
+++ lib/Sema/SemaDeclCXX.cpp
@@ -12399,11 +12399,6 @@ void Sema::DefineAmpCpuSerializeFunction(SourceLocation CurrentLocation,
              MethodEnd = FieldClassDecl->method_end();
              Method != MethodEnd; ++Method) {
           if((*Method)->getNameInfo().getAsString() == "__cxxamp_serialize") {
-            FieldDecl *F = dynamic_cast<FieldDecl*>(*Field);
-            assert(F && "F ERROR!");
-            Expr *FieldRef = BuildDeclRefExpr(F,FieldType,VK_LValue,Loc).get();
-            assert(FieldRef && "Reference to Field cannot fail!");
-  
             // Intentionally empty
             CXXScopeSpec CSS;
             LookupResult MemberLookup(*this, Field->getDeclName(), Loc,
@@ -12527,11 +12522,6 @@ void Sema::DefineAmpCpuSerializeFunction(SourceLocation CurrentLocation,
              MethodEnd = FieldClassDecl->method_end();
              Method != MethodEnd; ++Method) {
           if((*Method)->getNameInfo().getAsString() == "__cxxamp_serialize") {
-            FieldDecl *F = dynamic_cast<FieldDecl*>(*Field);
-            assert(F && "F ERROR!");
-            Expr *FieldRef = BuildDeclRefExpr(F,FieldType,VK_LValue,Loc).get();
-            assert(FieldRef && "Reference to Field cannot fail!");
-
             // Intentionally empty
             CXXScopeSpec CSS;
             LookupResult MemberLookup(*this, Field->getDeclName(), Loc,
