8bb5bd76130cc4d383095d6d0f664e3b5c0bc86f
Jack Chung <whchung@gmail.com>
2016-07-05 12:42:46 +0800

    [Driver] introduce HCCHostToolChain and HCCDeviceToolChain member variables
    These member variables would keep proper new ToolChain instances for HCC
    (C++AMP or HC) later on.

--- include/clang/Driver/Compilation.h
+++ include/clang/Driver/Compilation.h
@@ -49,6 +49,9 @@ class Compilation {
   std::multimap<Action::OffloadKind, const ToolChain *>
       OrderedOffloadingToolchains;
 
+  const ToolChain *HCCHostToolChain;
+  const ToolChain *HCCDeviceToolChain;
+
   /// The original (untranslated) input argument list.
   llvm::opt::InputArgList *Args;
 
@@ -160,6 +163,16 @@ public:
     ActiveOffloadMask |= OffloadKind;
     OrderedOffloadingToolchains.insert(
         std::make_pair(OffloadKind, DeviceToolChain));
+  }
+
+  const ToolChain *getHCCHostToolChain() const { return HCCHostToolChain; }
+  const ToolChain *getHCCDeviceToolChain() const { return HCCDeviceToolChain; }
+
+  void setHCCHostToolChain(const ToolChain *HostToolChain) {
+    HCCHostToolChain = HostToolChain;
+  }
+  void setHCCDeviceToolChain(const ToolChain *DeviceToolChain) {
+    HCCDeviceToolChain = DeviceToolChain;
   }
 
   const llvm::opt::InputArgList &getInputArgs() const { return *Args; }
