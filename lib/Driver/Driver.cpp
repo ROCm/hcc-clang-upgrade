b019cade231ec12c0093bd5910af155fb201645e
vpykhtin <valery.pykhtin@gmail.com>
2016-12-11 19:19:07 +0300

    Restore driver functionality after ToolSelector was introduced.

--- lib/Driver/Driver.cpp
+++ lib/Driver/Driver.cpp
@@ -3207,6 +3207,16 @@ public:
   /// dropping them. If no suitable tool is found, null will be returned.
   const Tool *getTool(const ActionList *&Inputs,
                       ActionList &CollapsedOffloadAction) {
+
+    auto JA = BaseAction;
+    if (IsHCHostAssembleJobAction(JA) || IsHCKernelAssembleJobAction(JA) ||
+      IsCXXAMPAssembleJobAction(JA) || IsCXXAMPCPUAssembleJobAction(JA) ||
+      IsCXXAMPBackendJobAction(JA) || IsCXXAMPCPUBackendJobAction(JA)) {
+      const ToolChain *DeviceTC = C.getSingleOffloadToolChain<Action::OFK_HCC>();
+      assert(DeviceTC && "HCC Device ToolChain is not set.");
+      return DeviceTC->SelectTool(*JA);
+    }
+
     //
     // Get the largest chain of actions that we could combine.
     //
