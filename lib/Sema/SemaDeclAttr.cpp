50ae0cc1e2ee76a5504ab2b1ac2eefd284479b7c
Aaron Enye Shi <enye.shi@gmail.com>
2019-02-14 18:31:45 +0000

    Remove extra if from old AlwaysInlineAttr

--- lib/Sema/SemaDeclAttr.cpp
+++ lib/Sema/SemaDeclAttr.cpp
@@ -6659,7 +6659,6 @@ static void handleDeviceAttr(Sema &S, Decl *D, const ParsedAttr &Attr) {
                CUDADeviceAttr(Attr.getRange(), S.Context,
                               Attr.getAttributeSpellingListIndex()));
   } else if (S.LangOpts.CPlusPlusAMP) {
-    if (!S.LangOpts.AMPCPU)
     D->addAttr(::new (S.Context) CXXAMPRestrictAMPAttr(Attr.getRange(),
           S.Context, Attr.getAttributeSpellingListIndex()));
   } else {
